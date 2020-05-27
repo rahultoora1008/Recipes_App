350ba37a1d6aa6712c72b3c4a9580a68f622d286
Rahul2Ra <rahultoora1008@gmail.com>
2020-05-27 18:45:48 -0400

    Updates

---
+++ precache-manifest.0f9c920f003ed64d1cc93c7810718068.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5848d0d5847c510d686d76003b65a17",
+    "url": "/Recipes_App/index.html"
+  },
+  {
+    "revision": "af9ed8ec629cc77baa97",
+    "url": "/Recipes_App/static/css/main.f1d49189.chunk.css"
+  },
+  {
+    "revision": "bde698819e06749e27f3",
+    "url": "/Recipes_App/static/js/2.cebf4e2d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Recipes_App/static/js/2.cebf4e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9ed8ec629cc77baa97",
+    "url": "/Recipes_App/static/js/main.d32d6e10.chunk.js"
+  },
+  {
+    "revision": "3529767885afe2e79954",
+    "url": "/Recipes_App/static/js/runtime-main.40809af4.js"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/Recipes_App/static/media/Product Sans Regular.eae9c18c.ttf"
+  },
+  {
+    "revision": "1cdf3e846aa478bab152451df3c8bbee",
+    "url": "/Recipes_App/static/media/chef.1cdf3e84.svg"
+  }
+]);
